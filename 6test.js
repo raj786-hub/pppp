6f003081b3dc85f42d4782f3051cd21cfa47ab89
raj786-hub <76934322+raj786-hub@users.noreply.github.com>
2021-01-11 19:44:19 +0530

    Add files via upload

---
+++ 6test.js
@@ -0,0 +1,18 @@
+import { Selector } from 'testcafe';
+
+fixture `New Fixture`
+    .page `https://dev-marketplace.claims-mp-dev.dev.us.insurance.dxc.com/claims/`;
+
+test('New Test', async t => {
+    await t
+        .click(Selector('header a').withText('LOGIN').nth(1))
+        .typeText('#userName', 'raj')
+        .typeText('#userPassword', '123@Mummy')
+        .click(Selector('button').withText('SIGN IN'))
+        .click(Selector('header a').withText('News & Events'))
+        .click(Selector('#mainContent a').withText('Conference Presentations'))
+        .click(Selector('#mainContent a').withText('Press Release News'))
+        .click(Selector('#mainContent a').withText('Webinars'))
+        .click(Selector('#mainContent a').withText('Newsletter'))
+        .click(Selector('header a').withText('LOGOUT').nth(1));
+});
